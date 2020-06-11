a66accb596a3a44704d2494c1d96997f58e0c044
qinzhiyonyan <1447312816@qq.com>
2020-06-11 20:45:06 +0800

    TodoList替换为无状态行数，提升性能

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,30 +1,31 @@
 import React from "react";
 import 'antd/dist/antd.css'
 import {Button, Input, List} from "antd";
-class TodoListUI extends React.Component{
-    render() {
-        return(
-            <div>
-                <div style={{margin: '10px'}}>
-                    <Input
-                        value={this.props.inputValue}
-                        placeholder="write something"
-                        style={{width: '250px', marginRight: '36px'}}
-                        onChange={this.props.changeInputValue}
-                    />
-                    <Button type="primary" onClick={this.props.addList}>增加</Button>
-                </div>
-                <List
-                    size="small"
-                    style={{marginLeft: '10px', width: '350px'}}
-                    header={<div>待处理事项</div>}
-                    bordered
-                    dataSource={this.props.list}
-                    renderItem={(item, index) => (<List.Item
-                        onClick={this.props.deleteItem.bind(this,index)}>{item}</List.Item>) }
+
+
+const TodoListUI = (props) =>{
+    return(
+        <div>
+            <div style={{margin: '10px'}}>
+                <Input
+                    value={props.inputValue}
+                    placeholder="write something"
+                    style={{width: '250px', marginRight: '36px'}}
+                    onChange={props.changeInputValue}
                 />
+                <Button type="primary" onClick={props.addList}>增加</Button>
             </div>
-        )
-    }
+            <List
+                size="small"
+                style={{marginLeft: '10px', width: '350px'}}
+                header={<div>待处理事项</div>}
+                bordered
+                dataSource={props.list}
+                renderItem={(item, index) => (<List.Item
+                    onClick={props.deleteItem.bind(this,index)}>{item}</List.Item>) }
+            />
+        </div>
+    )
 }
+
 export default TodoListUI
